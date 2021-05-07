10701498f28e434b82ab1f1a7dac08044cac059b
vladislav-khobot <vladislav.khobot@gmail.com>
2021-05-07 21:39:02 +0300

    add redux-tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { colorReducer } from './colors/reducer';
 
-const store = createStore(colorReducer);
+const store = createStore(
+  colorReducer, composeWithDevTools(
+  applyMiddleware())
+  );
 
 export { store };
